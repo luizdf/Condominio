b43e631f05b4f554dbd16b90144e30a4a8a16bb7
Luiz Antônio de Almeida <luizdf.com@gmail.com>
2017-01-13 20:08:46 -0200

    Commit

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,14 +1,10 @@
-package org.luiz.condominio.view.app;
 
-import org.apache.log4j.BasicConfigurator;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Start {
 
     public static void main(String[] args) throws Exception {
-        BasicConfigurator.configure();
-
         String webPort = System.getenv("PORT");
         if (webPort == null || webPort.isEmpty()) {
             webPort = "8080";
